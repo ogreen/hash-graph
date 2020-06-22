6459dac3f88edbfed1156ccb45af3b57b9664bc0
Oded Green <ogreen@nvidia.com>
2020-06-22 19:49:27 -0400

    Added indices to the table

--- hg.py
+++ hg.py
@@ -45,7 +45,7 @@ def countHashBins(hashA, counterArray, binSize):
 
 
 @cuda.jit
-def reOrderInput(inputA,hashA, d_InputAReOrdered, binSize, counterArray, prefixArray):
+def reOrderInput(inputA,hashA, d_InputAReOrdered, binSize, counterArray, prefixArray, indices, needIndices):
     start  = cuda.grid(1)
     stride = cuda.gridsize(1)
     size   = inputA.shape[0]
@@ -53,7 +53,8 @@ def reOrderInput(inputA,hashA, d_InputAReOrdered, binSize, counterArray, prefixA
         mybin = int(hashA[idx]//binSize)
         pos = cuda.atomic.add(counterArray,mybin,1) + prefixArray[mybin]
         d_InputAReOrdered[pos]=inputA[idx] # Todo store index
-
+        if(needIndices==True):
+            indices[pos]=idx
 
 @cuda.jit
 def reOrderHash(hashAReordered, counterArray):
@@ -123,7 +124,7 @@ class HashGraph:
   blocks_per_grid   = 512
 
 
-  def __init__(self, d_inputA, numBins=1<<14):
+  def __init__(self, d_inputA, indices=False, numBins=1<<14):
 
     self.numBins = numBins
     self.hashRange = (d_inputA.shape[0]) >> 2
@@ -131,10 +132,13 @@ class HashGraph:
 
     # self.d_table           = cuda.device_array(d_inputA.shape, dtype=inputA.dtype)
     # self.d_PrefixSum       = cuda.device_array(self.hashRange+1, dtype=np.uintc)
-    self.d_table, self.d_PrefixSum = self.build(d_inputA)
+    if (indices==False):
+        self.d_table, self.d_PrefixSum = self.build(d_inputA,False)
+    else:
+        self.d_table, self.d_PrefixSum, d_indices = self.build(d_inputA,True)
 
 
-  def build(self, d_input):
+  def build(self, d_input, indices=False):
     d_InputAReOrdered  = cuda.device_array(d_input.shape, dtype=inputA.dtype)
 
     d_HashA            = cuda.device_array(d_input.shape, dtype=np.uintc)
@@ -150,6 +154,9 @@ class HashGraph:
     d_table           = cuda.device_array(d_input.shape, dtype=inputA.dtype)
     d_PrefixSum       = cuda.device_array(self.hashRange+1, dtype=np.uintc)
 
+    # d_indices         = None
+    # if(indices==True):
+    d_indices = cuda.device_array(d_input.shape, dtype=np.uintc)
 
     start = time.time()
 
@@ -165,7 +172,7 @@ class HashGraph:
     d_BinPrefixSum = numba.cuda.to_device(cupy.cumsum(d_BinCounterArray,dtype=np.uintc))
 
     resetCounter[HashGraph.blocks_per_grid, HashGraph.threads_per_block](d_BinCounterArray)
-    reOrderInput[HashGraph.blocks_per_grid, HashGraph.threads_per_block](d_input,d_HashA,d_InputAReOrdered, self.binSize, d_BinCounterArray, d_BinPrefixSum)
+    reOrderInput[HashGraph.blocks_per_grid, HashGraph.threads_per_block](d_input,d_HashA,d_InputAReOrdered, self.binSize, d_BinCounterArray, d_BinPrefixSum, d_indices, indices)
 
     # continue;
     d_hashA = gdf.hash_columns(["b"])
@@ -186,10 +193,14 @@ class HashGraph:
     thisTime = time.time()-start
     print('Time taken = %2.6e seconds'%(thisTime))
     print('Rate = %.5e keys/sec'%((inputSize)/thisTime))
-    return d_table, d_PrefixSum
+    if(indices==False):
+        return d_table, d_PrefixSum
+    else:
+        return d_table, d_PrefixSum, d_indices
+
 
   def queryExistance(self, d_inputB):
-    d_tableB, d_PrefixSumB = self.build(d_inputB)
+    d_tableB, d_PrefixSumB, d_indicesB = self.build(d_inputB,True)
 
     # d_flagArray = cuda.device_array(d_inputB.shape, dtype=np.int8)
     d_flagArray = cupy.zeros(d_inputB.shape, dtype=np.int8)
@@ -201,7 +212,7 @@ class HashGraph:
     return d_flagArray
 
   def queryCount(self, d_inputB):
-    d_tableB, d_PrefixSumB = self.build(d_inputB)
+    d_tableB, d_PrefixSumB, d_indicesB = self.build(d_inputB,True)
 
     # d_flagArray = cuda.device_array(d_inputB.shape, dtype=np.int8)
     d_countArray = cupy.zeros(d_inputB.shape, dtype=np.int8)
@@ -213,7 +224,7 @@ class HashGraph:
     return d_countArray
 
 
-inputSize = 1<<23
+inputSize = 1<<28
 low = 0
 high = inputSize 
 # hashRange = inputSize >> 2
